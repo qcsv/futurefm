e79e6e8f3f64824f99ab82967353f60c2eabe971
cqst <cqst@cqstia.com>
2026-04-01 18:29:48 -0700

    comments

--- setup.sh
+++ setup.sh
@@ -4,16 +4,18 @@ echo "========> Setting up server"
 sudo apt-get -y install mpd apache2 php sqlite3 mpc
 
 
-adduser futurefum
-addgroup music-players
-sudo mkdir /opt/radio
-sudo chown futurefm /opt/radio
+# sudo adduser futurefum
+# sudo addgroup music-players
+#sudo mkdir /opt/radio
+#sudo chown futurefm /opt/radio
 
 
 # Deploy MPD Config file
-cp config/mpd.conf /etc/mpd.conf
+#cp config/mpd.conf /etc/mpd.conf
+
 # Deploy PHP file
-cp www/index.php /var/www/htdocs/index.php
+#cp www/index.php /var/www/htdocs/index.php
+
 # Start apache2
-sudo systemctl enable --now apache2
+#sudo systemctl enable --now apache2
 
