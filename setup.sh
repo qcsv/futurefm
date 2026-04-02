05cd7d66ff606a4bc70873ce4837bbd7888862dd
cqst <cqst@cqstia.com>
2026-04-01 18:24:18 -0700

    initial

--- setup.sh
+++ setup.sh
@@ -3,8 +3,17 @@
 echo "========> Setting up server"
 sudo apt-get -y mpd apache2 php sqlite3 mpc
 
-# Create MPD database (where?)
+
+adduser futurefum
+addgroup music-players
+sudo mkdir /opt/radio
+sudo chown futurefm /opt/radio
+
+
 # Deploy MPD Config file
+cp config/mpd.conf /etc/mpd.conf
 # Deploy PHP file
+cp www/index.php /var/www/htdocs/index.php
 # Start apache2
 sudo systemctl enable --now apache2
+
